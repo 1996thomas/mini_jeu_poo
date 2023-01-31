9b79e400cadedd55ddd38f42f25abcab5812a4cc
Thomas <thomas.kanthack@gmail.com>
2023-01-31 14:17:49 +0100

    V2.0

--- app.rb
+++ app.rb
@@ -7,29 +7,17 @@ require_relative 'lib/player'
 
 player1 = Player.new('José')
 player2 = Player.new('Sanchez')
+human = HumanPlayer.new('Moi')
 
-puts "A ma droite #{player1.name}"
-puts "A ma gauche #{player2.name}"
-player1.default_show_state
-player2.default_show_state
-puts "Let's get ready to rumble"
-puts '================================'
-puts "Et nous commençons avec #{player1.name} qui attaque #{player2.name}"
-player1.attack(player2)
-puts "C'est au tour de #{player2.name} de lancer une contre-attaque"
-player2.attack(player1)
 
-while player1.life_points >= 0 || player2.life_points >= 0
-  break if player1.life_points <= 0 || player2.life_points <= 0
 
-  puts '================================'
-  puts "Et c'est au tour de #{player1.name} d'attaquer #{player2.name}"
-  player1.attack(player2)
-  break if player1.life_points <= 0 || player2.life_points <= 0
 
-  puts "C'est maintenant à #{player2.name} de riposter"
-  player2.attack(player1)
 
-end
+
+
+
+
+
+
 
 binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -35,3 +35,47 @@ class Player
     puts "#{@name} est KO"
   end
 end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name)
+    super
+    @life_points = 100
+    @weapon_level = 1
+  end
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+    weapon_multiplicator = rand(1..6)
+    puts "Tu as récupérer une arme de niveau #{weapon_multiplicator}"
+    if weapon_multiplicator > @weapon_level
+      @weapon_level = weapon_multiplicator
+      puts "Bien joué tu as trouvé une arme plus puissante !"
+      return @weapon_level 
+    elsif weapon_multiplicator == @weapon_level 
+      puts "Mince, ton arme actuelle est du même niveau"
+    else puts "Dommage, ton arme actuelle est d'un niveau supérieur"
+    end
+  end
+
+  def search_health_pack
+    search_dice = rand(1..6)
+    case search_dice
+    when  1
+      puts "Tu n'as rien trouvé .. dommage"
+    when 2..5
+      heal = 50
+      @life_points = @life_points + heal
+      puts "Tu as récupérer 50 PV"
+    when 6
+      heal = 80
+      @life_points = @life_points + heal
+      puts "Tu as récupérer 80 PV"
+    end
+    @life_points > 100 ? @life_points = 100 : @life_points
+  end
+end
